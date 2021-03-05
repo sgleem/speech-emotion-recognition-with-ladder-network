c5c9f2501354dff4ceda3b130330243e90f36728
sgleem <sxl190087@utdallas.edu>
2021-03-05 11:59:21 -0600

    0305 fix

--- eval_ladder.py
+++ eval_ladder.py
@@ -137,5 +137,5 @@ if args.store_path is not None:
     lab_std = np.sqrt(test_set.lab_var)
     with open(args.store_path, 'w') as f:
         for utt_id, p in zip(test_set.utt_list, total_pred):
-            real_p = lab_std*p + lab_mu
+            real_p = lab_std*(p.cpu().numpy()) + lab_mu
             f.write(utt_id+"\t"+str(real_p[0])+"\t"+str(real_p[1])+"\t"+str(real_p[2])+"\n")
